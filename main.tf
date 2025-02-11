8328215e40fbb04a0ce3391093b2c5ffdce2ef01
g7crservice-nithin-s <120355084+g7crservice-nithin-s@users.noreply.github.com>
2025-02-11 16:05:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,7 +41,7 @@ resource "azurerm_storage_account" "example" {
     environment = var.environment
   }
 }
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "example2" {
   name                     = "storarageaccout1421"
   resource_group_name      = azurerm_resource_group.myRcGroup.name
   location                 = azurerm_resource_group.myRcGroup.location
